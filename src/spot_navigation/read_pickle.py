ac2b107f5e5be7410e8bb0d6ff56d3e8fc8a0294
Ming Jie See <jeremysee_2@hotmail.com>
2023-03-26 08:20:43 +0000

    rtabmap launch files

--- src/spot_navigation/read_pickle.py
+++ src/spot_navigation/read_pickle.py
@@ -1,12 +1,31 @@
 import pickle
+import typing
+from fiducial import Fiducial
 
 
 def read_fiducial_pickle():
     fiducials = pickle.load(open("fiducials_seen.pickle", "rb"))
-    print(type(fiducials))
-    for tag_id, data in fiducials.items():
-        print(tag_id)
-        print(data)
+
+    for tag_id, fiducial in fiducials.items():
+        print(f"tag_id: {tag_id}, occurences: {len(fiducial)}")
+        fiducial_data: typing.List[Fiducial] = fiducial
+        x = [i.filtered_fiducial_pose.pose.position.x for i in fiducial_data]
+        y = [i.filtered_fiducial_pose.pose.position.y for i in fiducial_data]
+        z = [i.filtered_fiducial_pose.pose.position.z for i in fiducial_data]
+        avg_x = sum(x) / len(x)
+        avg_y = sum(y) / len(y)
+        avg_z = sum(z) / len(z)
+        print(f"Filtered pose for tag {tag_id}")
+        print(f"avg_x: {avg_x:.2f}, avg_y: {avg_y:.2f}, avg_z: {avg_z:.2f}")
+
+        x = [i.fiducial_pose.pose.position.x for i in fiducial_data]
+        y = [i.fiducial_pose.pose.position.y for i in fiducial_data]
+        z = [i.fiducial_pose.pose.position.z for i in fiducial_data]
+        avg_x = sum(x) / len(x)
+        avg_y = sum(y) / len(y)
+        avg_z = sum(z) / len(z)
+        print(f"Unfiltered pose for tag {tag_id}")
+        print(f"avg_x: {avg_x:.2f}, avg_y: {avg_y:.2f}, avg_z: {avg_z:.2f}\n")
 
 
 if __name__ == "__main__":
