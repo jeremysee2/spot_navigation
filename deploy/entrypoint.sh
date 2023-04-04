3fe753557d4f2dba0a640fa83a80a402c01178ef
Ming Jie See <jeremysee_2@hotmail.com>
2023-04-04 16:59:53 +0000

    vnc setup

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+export DISPLAY=:20
+Xvfb :20 -screen 0 1366x768x16 &
+x11vnc -passwd 1234 -display :20 -N -forever -rfbport 21900 &
+
 source /ros/catkin_ws/devel/setup.sh && \
     roslaunch spot_navigation start_mapping.launch username:=admin password:=$SPOT_PASSWORD real_data:=$SPOT_REAL_DATA
