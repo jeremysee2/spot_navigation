84c2782d256ed9bad72932f482fe7e5032a629a5
Ming Jie See <jeremysee_2@hotmail.com>
2023-04-05 09:47:34 +0000

    start rviz

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -4,4 +4,4 @@ Xvfb :20 -screen 0 1366x768x16 &
 x11vnc -passwd 1234 -display :20 -N -forever -rfbport 21900 &
 
 source /ros/catkin_ws/devel/setup.sh && \
-    roslaunch spot_navigation start_mapping.launch username:=admin password:=$SPOT_PASSWORD real_data:=$SPOT_REAL_DATA & roslaunch spot_navigation view_map.launch
+    roslaunch spot_navigation start_mapping.launch username:=admin password:=$SPOT_PASSWORD real_data:=$SPOT_REAL_DATA
