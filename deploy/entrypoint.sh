1f58b5be4bec0563ea522ed70ec71c0ea7b005f3
Ming Jie See <jeremysee_2@hotmail.com>
2023-04-05 18:53:07 +0000

    env params to delete_db_on_start and start_vnc_server

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-export DISPLAY=:20
-Xvfb :20 -screen 0 1920x1080x16 &
-x11vnc -passwd 1234 -display :20 -N -forever -rfbport 21800 &
+
+if [ "$START_VNC_SERVER" == "true" ]
+    export DISPLAY=:20
+    Xvfb :20 -screen 0 1920x1080x16 &
+    x11vnc -passwd 1234 -display :20 -N -forever -rfbport 21800 &
+fi
 
 source /ros/catkin_ws/devel/setup.sh && \
-    roslaunch spot_navigation start_mapping.launch username:=admin password:=$SPOT_PASSWORD real_data:=$SPOT_REAL_DATA use_apriltag:=$USE_APRILTAG
+    roslaunch spot_navigation start_mapping.launch username:=$SPOT_USERNAME password:=$SPOT_PASSWORD \
+    real_data:=$SPOT_REAL_DATA use_apriltag:=$USE_APRILTAG delete_db:=$DELETE_DB_ON_START
