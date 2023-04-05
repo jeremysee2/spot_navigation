163058f9db0b3a4b1daeb117ff9697f3217fe73f
Ming Jie See <jeremysee_2@hotmail.com>
2023-04-05 15:06:32 +0000

    apriltag

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export DISPLAY=:20
-Xvfb :20 -screen 0 1366x768x16 &
-x11vnc -passwd 1234 -display :20 -N -forever -rfbport 21900 &
+Xvfb :20 -screen 0 1920x1080x16 &
+x11vnc -passwd 1234 -display :20 -N -forever -rfbport 21800 &
 
 source /ros/catkin_ws/devel/setup.sh && \
-    roslaunch spot_navigation start_mapping.launch username:=admin password:=$SPOT_PASSWORD real_data:=$SPOT_REAL_DATA
+    roslaunch spot_navigation start_mapping.launch username:=admin password:=$SPOT_PASSWORD real_data:=$SPOT_REAL_DATA use_apriltag:=$USE_APRILTAG
